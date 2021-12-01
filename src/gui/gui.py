7d1e53a0bd1772e6bf88bf788ad492fb53e83f55
Bruno Bacelar <bruno.bacelar94@hotmail.com>
2021-11-30 23:42:50 -0300

    added login screen

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,6 +1,8 @@
+from tkinter import messagebox
 from models.cliente import Cliente
 from db.database import criar_cliente
 import tkinter as tk
+from tkinter.messagebox import *
 import os
 
 def init_gui(nome, cpf, saldo):
@@ -35,25 +37,69 @@ def init_gui(nome, cpf, saldo):
 
     window.mainloop()
 
-def criar_janela_login():
-    global tela_login
-    tela_login = tk.Tk('Login')
-    tela_login.geometry('300x250')
-    tela_login.resizable(height=None, width=None)
+def criar_janela_inicio():
+    global tela_inicio
+    tela_inicio = tk.Tk('Login')
+    tela_inicio.geometry('300x250')
+    tela_inicio.resizable(height=None, width=None)
 
     intro_lbl = tk.Label(text="Banco do Bruhsil", bg="blue", width="300", height="2", font=("Calibri", 13), fg='white').pack()
     tk.Label(text='').pack()
-    login_btn = tk.Button(text='Login', height='2', width='30')
+    login_btn = tk.Button(text='Login', height='2', width='30', command=__criar_janela_login)
     login_btn.pack()
     tk.Label(text='').pack()
 
-    register_btn = tk.Button(text='Cadastrar', height='2', width='30', command=criar_janela_cadastro)
+    register_btn = tk.Button(text='Cadastrar', height='2', width='30', command=__criar_janela_cadastro)
     register_btn.pack()
 
-    tela_login.mainloop()
+    tela_inicio.mainloop()
 
-def criar_janela_cadastro():
-    tela_cadastro = tk.Toplevel(tela_login)
+def __criar_janela_login():
+    global tela_login
+    tela_login = tk.Toplevel(tela_inicio)
+    tela_login.title('Realizar login')
+    tela_login.geometry('300x250')
+
+    global usuario_verificar
+    global rg_verificar
+
+    usuario_verificar = tk.StringVar()
+    rg_verificar = tk.StringVar()
+
+    tk.Label(tela_login, text="Insira suas credenciais", bg="blue", fg='white').pack()
+    tk.Label(tela_login, text="").pack()
+
+    usuario_lbl = tk.Label(tela_login, text='Usuario *')
+    usuario_lbl.pack()
+
+    global usuario_ent_login
+    usuario_ent_login = tk.Entry(tela_login, textvariable=usuario_verificar)
+    usuario_ent_login.pack()
+
+    rg_lbl = tk.Label(tela_login, text='RG *')
+    rg_lbl.pack()
+
+    global rg_ent_login
+    rg_ent_login = tk.Entry(tela_login, textvariable=rg_verificar)
+    rg_ent_login.pack()
+
+    tk.Label(tela_login, text="").pack()
+
+    login_btn = tk.Button(tela_login, text='Login', height='2', width='30', command=__realizar_login)
+    login_btn.pack()
+
+
+def __realizar_login():
+    info_usuario = usuario_ent_login.get()
+    info_rg = rg_ent_login.get()
+    cliente = Cliente(info_usuario, info_rg)
+    # Inserir bagulhos para logar cliente
+    messagebox.showinfo(title='Login', message='Login feito com sucesso!')
+    tela_login.destroy()
+
+def __criar_janela_cadastro():
+    global tela_cadastro
+    tela_cadastro = tk.Toplevel(tela_inicio)
     tela_cadastro.title('Cadastro')
     tela_cadastro.geometry('300x250')
 
@@ -66,27 +112,30 @@ def criar_janela_cadastro():
     usuario_lbl = tk.Label(tela_cadastro, text='Usuario *')
     usuario_lbl.pack()
 
-    global usuario_ent
-    usuario_ent = tk.Entry(tela_cadastro, textvariable=nome)
-    usuario_ent.pack()
+    global usuario_ent_cadastro
+    usuario_ent_cadastro = tk.Entry(tela_cadastro, textvariable=nome)
+    usuario_ent_cadastro.pack()
 
     rg_lbl = tk.Label(tela_cadastro, text='RG *')
     rg_lbl.pack()
 
-    global rg_ent
-    rg_ent = tk.Entry(tela_cadastro, textvariable=rg)
-    rg_ent.pack()
+    global rg_ent_cadastro
+    rg_ent_cadastro = tk.Entry(tela_cadastro, textvariable=rg)
+    rg_ent_cadastro.pack()
 
     tk.Label(tela_cadastro, text="").pack()
 
-    cadastro_btn = tk.Button(tela_cadastro, text='Confirmar', height='2', width='30', command=cadastrar_usuario)
+    cadastro_btn = tk.Button(tela_cadastro, text='Confirmar', height='2', width='30', command=__cadastrar_usuario)
     cadastro_btn.pack()
 
-def cadastrar_usuario():
-    info_usuario = usuario_ent.get()
-    info_rg = rg_ent.get()
+def __cadastrar_usuario():
+    info_usuario = usuario_ent_cadastro.get()
+    info_rg = rg_ent_cadastro.get()
     cliente = Cliente(info_usuario, info_rg)
     criar_cliente(cliente)
+    messagebox.showinfo(title='Cadastro confirmado', message='Cliente cadastrado com sucesso!')
+    tela_cadastro.destroy()
+    #tela_cadastro.update()
 
 def transferir():
     pass
